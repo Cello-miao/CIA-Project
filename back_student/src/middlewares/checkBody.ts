3e32cf05c0dbbbfa55a42ac2c4117cf14098970b
engrahmadmirza <ahmedmirza@elgrocer.com>
2026-05-27 23:17:53 +0200

    midleware fix and handle CORS

--- back_student/src/middlewares/checkBody.ts
+++ back_student/src/middlewares/checkBody.ts
@@ -8,9 +8,11 @@ export const validateEmpty = (
   if (
     req.body === undefined ||
     req.body.username === undefined ||
-    req.body.password === undefined
+    req.body.password === undefined ||
+    req.body.username === '' ||
+    req.body.password === ''
   ) {
-    return res.status(400).send();
+    return res.status(400).send('Invalid request body');
   }
   // if string value is longer than 0, continue with next function in route
   next();
