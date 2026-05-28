d91b306cb1ed665c0dc9497f0fcedc089a476bae
Xinxin <xinxin.miao@epitech.eu>
2026-05-28 23:48:55 +0200

    fix: make New Relic optional, disable if license key not provided

--- back_student/newrelic.js
+++ back_student/newrelic.js
@@ -1,6 +1,15 @@
 'use strict'
-exports.config = {
-  app_name: ['cia-backend-api'],
-  license_key: process.env.NEW_RELIC_LICENSE_KEY,
-  logging: { level: 'info' }
+
+// Only enable New Relic if license key is provided
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+  exports.config = {
+    app_name: ['cia-backend-api'],
+    license_key: process.env.NEW_RELIC_LICENSE_KEY,
+    logging: { level: 'info' }
+  }
+} else {
+  // Disable New Relic by setting enabled to false
+  exports.config = {
+    enabled: false
+  }
 }
